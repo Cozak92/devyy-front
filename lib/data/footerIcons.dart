c38e39353aa7b46e82490fdb19bb31d127d51ff1
신승혁 <Cozak92@gmail.com>
2022-01-15 10:24:51 +0900

    ✨ icon 관련된 data 생성

---
+++ lib/data/footerIcons.dart
@@ -0,0 +1,6 @@
+const List footerItemIcons = [
+  {"icons" : "assets/images/footer/profile.png", "size" : 45.0 },
+  {"icons" : "assets/images/footer/search.png", "size" : 45.0 },
+  {"icons" : "assets/images/footer/chat.png", "size" : 45.0 },
+  {"icons" : "assets/images/footer/settings.png", "size" : 45.0 }
+];
